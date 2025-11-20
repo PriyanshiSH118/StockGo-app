aef65d3628fb03bd4e751f80d7d5c3949b969787
Priyanshi Sharma <chisaaa1176@gmail.com>
2025-11-20 09:35:59 +0530

    Update project (header and other changes)

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
      <header className="sticky top-0 header">
          <div className="container header-wrapper">
             <Link href="/">
-                <Image src="/assets/icons/logo.svg" alt="StockGo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
+                <Image src="/assets/icons/stock.svg" alt="StockGo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
             </Link>
              <nav className="hidden sm:block">
                  <NavItems/>
