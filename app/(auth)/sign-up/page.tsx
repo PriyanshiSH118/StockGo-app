ade0a381ed564d0e0a717e003152463ea34966c7
Priyanshi Sharma <chisaaa1176@gmail.com>
2025-11-25 10:27:53 +0530

    Implement Auth UI & Layout

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,110 @@
+'use client';
+import React from "react";
+import {useForm} from "react-hook-form";
+import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
+import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
+import {CountrySelectField} from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
+
+const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors,isSubmitting },
+    } = useForm<SignUpFormData>({
+        defaultValues:{
+            fullName:'',
+            email:'',
+            password:'',
+            country:'India',
+            investmentGoals:'Growth',
+            riskTolerance:'Medium',
+            preferredIndustry:'Technology'
+        },mode:'onBlur'
+    })
+     const onSubmit =async(data:SignUpFormData)=>{
+        try{
+            console.log(data);
+        } catch(e){
+            console.log(e);
+        }
+
+     }
+
+
+  return (
+    <>
+        <h1 className="form-title">Sign Up & Personalize</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+           <InputField
+               name="fullName"
+               label="Full Name"
+               placeholder="Alesia"
+               register={register}
+               error={errors.fullName}
+               validation={{required: 'Full name is required', minLength: 2}} />
+
+            <InputField
+                name="email"
+                label="Email"
+                placeholder="Alesia@gamil.com"
+                register={register}
+                error={errors.email}
+                validation={{
+                    required: 'Email is required',
+                    minLength: 2,
+                    pattern: /^\w+@\w+\.\w+$/,
+                    message: 'Email is required'
+                }}/>
+
+            <InputField
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+                type="password"
+                register={register}
+                error={errors.password}
+                validation={{required: 'Password is required', minLength: 8}}/>
+
+            <CountrySelectField name="country" control={control} error={errors.country} required label="country"/>
+
+
+            <SelectField
+                name="investmentGoals"
+                label="Investment Goals"
+                placeholder="Select your investment goal"
+                options={INVESTMENT_GOALS}
+                control={control}
+                error={errors.investmentGoals}
+                required
+            />
+            <SelectField
+                name="riskTolrence"
+                label="RiskTolrence"
+                placeholder="Medium"
+                options={RISK_TOLERANCE_OPTIONS}
+                control={control}
+                error={errors.riskTolerance}
+                required
+            />
+            <SelectField
+                name="preferredIndustry"
+                label="Investment Goals"
+                placeholder="Select your investment goal"
+                options={PREFERRED_INDUSTRIES}
+                control={control}
+                error={errors.preferredIndustry}
+                required
+            />
+            <button type="submit" disabled={isSubmitting} className="blue-btn w-full mt-5">
+                {isSubmitting ?'Creating account' : 'Start Your Investing Journey'}
+            </button>
+            <FooterLink text="Already have an account?" linkText="Sign in" href="/sign-in"/>
+        </form>
+    </>
+  );
+};
+
+export default SignUp;
